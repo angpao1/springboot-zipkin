e1fde6bf95e37905d3674c85a65f82bbeebf55c1
angpao <angpao.kittimasak@gmail.com>
2019-05-12 06:50:10 +0700

    Create User Model

---
+++ src/main/java/com/example/microserviceregister/models/User.java
@@ -0,0 +1,39 @@
+package com.example.microserviceregister.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String usernane;
+    private String password;
+
+    public User() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsernane() {
+        return usernane;
+    }
+
+    public void setUsernane(String usernane) {
+        this.usernane = usernane;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
