5334f3f7efa2d90f7c8ca8a8029581eb186b374e
angpao <angpao.kittimasak@gmail.com>
2019-05-12 06:48:26 +0700

    Initial Project

---
+++ src/test/java/com/example/microserviceregister/MicroserviceRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microserviceregister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceRegisterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
